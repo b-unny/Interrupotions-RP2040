0a94e61003bdaa7e61cb27bc4c292782ea5c31c1
Camilla Matos <146495739+b-unny@users.noreply.github.com>
2025-02-03 00:01:15 -0300

    Update lib.h
    adding colors defined

--- lib.h
+++ lib.h
@@ -1,18 +1,21 @@
 #ifndef LIB_H
 #define LIB_H
 
+#define PASTEL_PINK 0xFFD1DC  // RGB(255, 209, 220)
+#define BLK 0x000000
+
 #include"pico/stdlib.h"
 #include"hardware/gpio.h"
 #include"hardware/pio.h"
 #include"ws2812.pio.h"
 
-void init_led_rgb()
-void blink_red_led()
-void init_buttons() 
-void button_a_isr(uint gpio, uint32_t event)
-void button_b_isr(uint gpio, uint32_t events) 
-void init_led_matrix() 
-void update_display(int number) 
-void set_led_matrix(int row, int col, uint32_t color)
+void init_led_rgb();
+void blink_red_led();
+void init_buttons();
+void button_a_isr(uint gpio, uint32_t event);
+void button_b_isr(uint gpio, uint32_t events); 
+void init_led_matrix();
+void update_display(int number); 
+void set_led_matrix(int row, int col, uint32_t color);
 
 #endif
