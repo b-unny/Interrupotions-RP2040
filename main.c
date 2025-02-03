bf6159d3e799d7328d44f5ee0320dacf9bb46631
Camilla Matos <146495739+b-unny@users.noreply.github.com>
2025-02-03 00:04:36 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,14 @@
-#include<stdio.h>
+#include "lib.h"
+#include "pico/stdlib.h"
+
+int main() 
+{
+    stdio_init_all();
+    init_led_rgb();
+    init_led_matrix();
+    init_buttons();
+    
+    while (true)  blink_red_led(); 
+
+    return 0;
+}
