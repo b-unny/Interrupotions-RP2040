83c94a78d3d2abde932c4a0006579bc7871c7a17
Camilla Matos <146495739+b-unny@users.noreply.github.com>
2025-02-07 20:15:22 -0300

    Update lib.c

--- lib.c
+++ lib.c
@@ -1,6 +1,7 @@
 #include "lib.h"
 
 volatile int count  = 0;
+volatile uint32_t last_interrupt_time = 0;
 
 void init_led_rgb() 
 {
@@ -15,9 +16,9 @@ void init_led_rgb()
 void blink_red_led()
 {
     gpio_put(LED_R, true);
-    slepp_ms(100);
+    sleep_ms(100);
     gpio_put(LED_R, false);
-    slepp_ms(100);
+    sleep_ms(100);
 }
 
 void init_buttons() 
@@ -25,31 +26,48 @@ void init_buttons()
     gpio_init(BUTTON_A);
     gpio_set_dir(BUTTON_A, GPIO_IN);
     gpio_pull_up(BUTTON_A);
-    gpio_set_irq_edge_rise(BUTTON_A, button_a_isr);
+    gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_RISE, true, button_a_isr);
     
     gpio_init(BUTTON_B);
     gpio_set_dir(BUTTON_B, GPIO_IN);
     gpio_pull_up(BUTTON_B);
-    gpio_set_irq_edge_rise(BUTTON_B, button_b_isr);
-}
+    gpio_set_irq_enabled_with_callback(BUTTON_B, GPIO_IRQ_EDGE_RISE, true, button_b_isr);
+}    
 
-// handler botao A
 void button_a_isr(uint gpio, uint32_t event)
-{
+{ 
+    uint32_t current_time = to_ms_since_boot(get_absolute_time());
+    if (current_time - last_interrupt_time < 200) return; 
+    last_interrupt_time = current_time;
+    
     count--;
     if (count <  0) count = 9;
     update_display(count);
 }
 
-//handler botao B
 void button_b_isr(uint gpio, uint32_t events) 
 {
+    uint32_t current_time = to_ms_since_boot(get_absolute_time());
+    if (current_time - last_interrupt_time < 200) return;
+    last_interrupt_time = current_time;
+
     count++;
     if (count > 9) count = 0;
     update_display(count);
 }
 
-//TODO ver expliccao mais detalhada disso aqui, tendi nao
+
+//TODO ver expliccao mais detalhada das funçoes abaixo, tendi nao T-T
+
+static uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) 
+{
+    return (r << 8) | (g << 16) | b;
+}
+static void put_pixel(uint32_t pixel_grb)
+{
+    pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);  
+    // Envia para o pio0, estado de máquina 0
+}
 void init_led_matrix() 
 {
     PIO pio = pio0;
@@ -58,35 +76,88 @@ void init_led_matrix()
     ws2812_program_init(pio, sm, offset, MATRIX_PIN, 800000, false);
 }
 
-// Atualiza a matriz para exibir um número específico
-void update_display(int number) 
+void set_led_matrix(int index, uint32_t color) 
 {
-    static const uint8_t numbers[10][5][5] = { // de 0 a 9
-        {{0,1,1,1,0},{1,0,0,0,1},{1,0,0,0,1},{1,0,0,0,1},{0,1,1,1,0}}, 
-        {{0,0,1,0,0},{0,1,1,0,0},{0,0,1,0,0},{0,0,1,0,0},{0,1,1,1,0}}, 
-        {{0,1,1,1,0},{0,0,0,1,0},{0,0,1,0,0},{0,1,0,0,0},{0,1,1,1,0}},
-        {{0,1,1,1,0},{0,0,0,1,0},{0,0,1,1,0},{0,0,0,1,0},{0,1,1,1,0}}, 
-        {{0,0,0,1,0},{0,0,1,1,0},{0,1,0,1,0},{1,1,1,1,1},{0,0,0,1,0}}, 
-        {{0,1,1,1,0},{0,1,0,0,0},{0,1,1,1,0},{0,0,0,1,0},{0,1,1,1,0}}, 
-        {{0,1,1,1,0},{0,1,0,0,0},{0,1,1,1,0},{0,1,0,1,0},{0,1,1,1,0}}, 
-        {{0,1,1,1,0},{0,0,0,1,0},{0,0,1,0,0},{0,1,0,0,0},{0,1,0,0,0}}, 
-        {{0,1,1,1,0},{0,1,0,1,0},{0,1,1,1,0},{0,1,0,1,0},{0,1,1,1,0}}, 
-        {{0,1,1,1,0},{0,1,0,1,0},{0,1,1,1,0},{0,0,0,1,0},{0,1,1,1,0}}  
-};
-    
-    for (int row = 0; row < 5; row++) {
-        for (int col = 0; col < 5; col++) {
-            set_led_matrix(row, col, numbers[number][row][col] ? PASTEL_PINK : BLK);
-        }
+    if (index >= 0 && index < NUM_LEDS) 
+    {
+        put_pixel(color);
     }
+}
+ 
+void update_display(int number)
+{
+   uint32_t pastel_pink = urgb_u32(
+    (uint8_t)(193 * BRIGHTNESS), 
+    (uint8_t)(255 * BRIGHTNESS), 
+    (uint8_t)(182 * BRIGHTNESS)
+);
+
+    static const uint8_t numbers[10][NUM_LEDS] ={
+        {   0, 1, 1, 1, 0, 
+            0, 1, 0, 1, 0, 
+            0, 1, 0, 1, 0, 
+            0, 1, 0, 1, 0, 
+            0, 1, 1, 1, 0}, // 0
+
+        {   0, 0, 1, 0, 0, 
+            0, 0, 1, 0, 0, 
+            0, 0, 1, 0, 0, 
+            0, 1, 1, 0, 0, 
+            0, 0, 1, 0, 0}, // 1
+
+        {   0, 1, 1, 1, 0, 
+            0, 1, 0, 0, 0, 
+            0, 1, 1, 1, 0, 
+            0, 0, 0, 1, 0, 
+            0, 1, 1, 1, 0},  // 2
+
+        {   0, 1, 1, 1, 0, 
+            0, 0, 0, 1, 0, 
+            0, 1, 1, 1, 0, 
+            0, 0, 0, 1, 0, 
+            0, 1, 1, 1, 0}, //3
+
+        {   0, 1, 0, 0, 0, 
+            0, 0, 0, 1, 0, 
+            0, 1, 1, 1, 0, 
+            0, 1, 0, 1, 0, 
+            0, 1, 0, 1, 0}, //4
+
+        {   0, 1, 1, 1, 0, 
+            0, 0, 0, 1, 0, 
+            0, 1, 1, 1, 0, 
+            0, 1, 0, 0, 0, 
+            0, 1, 1, 1, 0}, //5
+        
+        {   0, 1, 1, 1, 0, 
+            0, 1, 0, 1, 0, 
+            0, 1, 1, 1, 0, 
+            0, 1, 0, 0, 0, 
+            0, 1, 1, 1, 0}, //6
+
+        {   0, 1, 0, 0, 0, 
+            0, 0, 0, 1, 0, 
+            0, 1, 0, 0, 0, 
+            0, 0, 0, 1, 0, 
+            0, 1, 1, 1, 0}, //7
+
+        {   0, 1, 1, 1, 0, 
+            0, 1, 0, 1, 0, 
+            0, 1, 1, 1, 0, 
+            0, 1, 0, 1, 0, 
+            0, 1, 1, 1, 0}, //8
+
+        {   0, 1, 0, 0, 0, 
+            0, 0, 0, 1, 0, 
+            0, 1, 1, 1, 0, 
+            0, 1, 0, 1, 0, 
+            0, 1, 1, 1, 0} //9   
+    };
+
+    
+    for (int index = 0; index < NUM_LEDS; index++) 
+        set_led_matrix(numbers[number][index] ?  pastel_pink : 0);
 }
 
-// Define um LED específico na matriz
-void set_led_matrix(int row, int col, uint32_t color) {
-    int index = row * 5 + col;
-    if (index >= 0 && index < 25) {
-        put_pixel(urgb_u32((color >> 16) & 0xFF, (color >> 8) & 0xFF, color & 0xFF));
-    }
-}
 
 
